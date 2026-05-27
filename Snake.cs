ec434cf2e75af13db286eb57161b748e51c4788d
Eunice Oladele <euniceoladele0@gmail.com>
2026-05-26 20:43:06 -0400

    Added Snake Subclass

---
+++ Snake.cs
@@ -0,0 +1,32 @@
+public class Snake : Animal
+{
+    private double length;
+    private bool venomous;
+
+    // constructor
+    public Snake(int id, string name, int age, string position, double length, bool venomous)
+    : base(id, name, age, position)
+    {
+        this.length() = length;
+        this.venomous() = venomous;
+    }
+
+    // setters and getters
+    public double Length
+    {
+        get { return length; }
+        set { length = value; }
+    }
+    public double Venomous
+    {
+        get { return venomous;}
+        set { venomous = value; }
+    }
+
+    // custom ToString method for Snake class
+    public override String ToString()
+    {
+        return base.ToString() + $", Length={length:F2}m, Venomous={venomous}";
+    }
+
+}
